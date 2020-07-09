1fcc60948bd8fca5484877d92f18ca132f1bee6d
Hotdoy <rouleau.vincent@gmail.com>
2020-07-09 19:53:20 -0400

    dump

--- js/collection.js
+++ js/collection.js
@@ -3,15 +3,11 @@
 	'use strict';
 
 	const InitCollection = function(){
-		let sel = '.collection .swiper-container';
-		let el = document.querySelectorAll(sel);
-		for (let e of el) {
-			let id = e.id;
-			let swiper = new Swiper('#'+id, {
-				slidesPerView: 'auto',
-				spaceBetween: 30,
-			});
-		}
+		let swiper = new Swiper('.module.collection .swiper-container', {
+			slidesPerView: 'auto',
+			spaceBetween: 20,
+		});
+		
 	}
 
 	InitCollection();
