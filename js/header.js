a7bfbfa966afa127852d5d14f0dbc992bcfaf52d
Hotdoy <rouleau.vincent@gmail.com>
2020-07-16 19:17:38 -0400

    a buttload

--- js/header.js
+++ js/header.js
@@ -4,29 +4,28 @@
 
 	const InitHeader = function() {
 		const b = document.body;
-		const onTriggers = document.getElementsByClassName("header__toggle-on");
-		const offTriggers = document.getElementsByClassName("header__toggle-off");
+		const onTriggers = document.getElementsByClassName("header__on");
+		const offTriggers = document.getElementsByClassName("header__off");
+		let lastOffset = 0;
 
 		for (let onTrigger of onTriggers) {
 			onTrigger.addEventListener("click", function(){
-				b.classList.add("header--deployed");
+				b.classList.add("header--on");
 			})
 		}
 
 		for (let offTrigger of offTriggers) {
 			offTrigger.addEventListener("click", function(){
-				b.classList.remove("header--deployed");
+				b.classList.remove("header--on");
 			})
 		}
 
-		window.addEventListener("scroll", function(){
-		   	let currentOffset = window.pageYOffset || document.documentElement.scrollTop;
-		   	if (window.pageYOffset > 200){
-				b.classList.add("header--scrolled"); 
-		   	} else if (window.pageYOffset < 200) {
-		    	b.classList.remove("header--scrolled");
-		   	}
-		}, false);
+		window.addEventListener('resize', function(){
+			if (window.innerWidth > 735 && b.classList.contains("header--on")) {
+				b.classList.remove("header--on");
+			}
+		});
+
 	}
 
 	InitHeader();
