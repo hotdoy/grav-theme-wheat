c56dbbd70d04db853234819be7240d95708d6ee0
Vincent <rouleau.vincent@gmail.com>
2020-07-31 07:50:27 -0400

    added history-back support for links

--- js/links.js
+++ js/links.js
@@ -7,12 +7,31 @@
 		const links = document.querySelectorAll('a');
 		links.forEach(link => {
 
+			// get the href
 			let href = link.getAttribute('href');
+
+			// external
 			if (!!href && href.match('^http')) {
 				link.setAttribute('target', '_blank');
 				link.setAttribute('rel', 'noopener');
 			}
-			
+
+			// go back in history if referrer includes the href
+			// The href should usially be the index of your site and will be used as fallback
+			else if (link.hasAttribute('data-history-back')) {
+				console.log('back');
+				link.addEventListener('click',(event) => {
+					event.preventDefault();
+					if (document.referrer.includes(href)) {
+						history.back();
+					} else {
+						window.location.href = href;
+					}
+					body.classList.add('navigating');
+				});
+			}
+
+			// internal
 			else if (!!href && href.match('^/')) {
 				link.addEventListener('click', function(event){
 					body.classList.add('navigating');
