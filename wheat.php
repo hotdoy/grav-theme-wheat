635bca742ebdaa7f9abcf09f86f6cdc4ad41f6bb
Vincent <rouleau.vincent@gmail.com>
2020-08-05 07:19:17 -0400

    added missing admin css utils

--- wheat.php
+++ wheat.php
@@ -5,5 +5,10 @@ use Grav\Common\Theme;
 
 class Wheat extends Theme
 {
-    // Access plugin events in this class
+	public function onTwigSiteVariables()
+    {
+        if ($this->isAdmin()) {
+            $this->grav['assets']->add('theme://css/admin.css');
+        }
+    }
 }
