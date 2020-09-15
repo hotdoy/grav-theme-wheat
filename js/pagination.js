120ad9ce4c74c69f929280da07b807f74960a70c
Vincent <rouleau.vincent@gmail.com>
2020-09-15 18:12:22 -0400

    Building bocks for a better pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,54 +1,54 @@
 const Pagination = {
 
-	Request: function(ctn, btn, url) {
+	ctn: {},
+	btn: {},
+	url: {},
+	result: {},
+
+	Request: function() {
 		let request = new XMLHttpRequest();
 		request.responseType = 'document';
-		request.open('GET', url, true);
+		request.open('GET', Pagination.url, true);
 		request.onload = function() {
-
 			if (this.status >= 200 && this.status < 400) {
-				
-				// Get new articles and append them to the pahination ctn
-				let articles = this.response.querySelector('[data-pagination-ctn]').childNodes;
-				for (var i = articles.length - 1; i >= 0; i--) {
-					ctn.appendChild(articles[i]);
-				}
-
-				// Get new pagination button and replace the old one
-				let newBtn = this.response.querySelector('[data-pagination-btn]');
-				if (!!newBtn) {
-					btn.parentNode.replaceChild(newBtn, btn);
-				} else {
-					btn.parentNode.remove();
-				}
-
-				// Init next pagination
-				Pagination.Init();
-				
+				Pagination.result = this.response;
+				Pagination.AppendResult();			
 			} else {
 				console.log('PAGINATION - SERVER STATUS: ' + this.status);
 			}
-
 		};
-
 		request.onerror = function() {
-			console.log('PAGINATION - ERROR');
+			console.log('PAGINATION - ERROR ' + this.status);
 		};
-
 		request.send();
+	},
+
+	AppendResult: function () {
+		const articles = Pagination.result.querySelector('[data-pagination-ctn]').childNodes;
+		for (var i = articles.length - 1; i >= 0; i--) {
+			Pagination.ctn.appendChild(articles[i]);
+		}
+		let newBtn = Pagination.result.querySelector('[data-pagination-btn]');
+		if (!!newBtn) {
+			Pagination.btn.parentNode.replaceChild(newBtn, Pagination.btn);
+		} else {
+			Pagination.btn.parentNode.remove();
+		}
+		Pagination.Init();
 	},
 
 
 	Init: function() {
-		const ctn = document.querySelector('[data-pagination-ctn]');
-		const btn = document.querySelector('[data-pagination-btn]');
-		const url = btn.getAttribute('href');
-
-		if (!!ctn && !!btn && !!url) {
-			btn.addEventListener('click',(event) => {
-				event.preventDefault();
-				Pagination.Request(ctn, btn, url);
-			});
+		Pagination.ctn = document.querySelector('[data-pagination-ctn]');
+		Pagination.btn = document.querySelector('[data-pagination-btn]');
+		if (!!Pagination.btn) {
+			Pagination.url = Pagination.btn.getAttribute('href');
+			if (!!Pagination.ctn && !!Pagination.url) {
+				Pagination.btn.addEventListener('click',(event) => {
+					event.preventDefault();
+					Pagination.Request();
+				});
+			}
 		}
 	},
 }
