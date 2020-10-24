c34f1b5093972ab9095175a36ca9c002945a9434
Vincent <rouleau.vincent@gmail.com>
2020-10-24 11:01:26 -0400

    cleanup map js

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
-function initMap() {
-  const ctns = document.querySelectorAll('[id^="map-"]');
+function initMap () {
+  const ctns = document.querySelectorAll('[id^="map-"]')
   for (let ctn of ctns) {
-    let zoom = ctn.dataset.zoom ? parseInt(ctn.dataset.zoom) : "";
-    let markers = ctn.dataset.markers ? JSON.parse(ctn.dataset.markers) : "";
-    let styles = ctn.dataset.styles ? JSON.parse(ctn.dataset.styles) : "";
-    let marker, i;
-    let bounds = new google.maps.LatLngBounds();
+    const zoom = ctn.dataset.zoom ? parseInt(ctn.dataset.zoom) : ''
+    const markers = ctn.dataset.markers ? JSON.parse(ctn.dataset.markers) : ''
+    const styles = ctn.dataset.styles ? JSON.parse(ctn.dataset.styles) : ''
+    let marker, i
+    const bounds = new google.maps.LatLngBounds()
 
-    let map = new google.maps.Map(ctn, {
+    const map = new google.maps.Map(ctn, {
       zoom: zoom,
       center: new google.maps.LatLng(markers[0][1], markers[0][2]),
       styles: styles || undefined,
@@ -16,53 +16,53 @@ function initMap() {
       streetViewControl: false,
       mapTypeControl: false,
       scrollwheel: false,
-      mapTypeId: "roadmap",
-      draggable: true,
-    });
+      mapTypeId: 'roadmap',
+      draggable: true
+    })
 
     for (i = 0; i < markers.length; i++) {
-      let title = markers[i][0];
-      let position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-      let url = markers[i][3];
-      let icon = markers[i][4];
+      const title = markers[i][0]
+      const position = new google.maps.LatLng(markers[i][1], markers[i][2])
+      const url = markers[i][3]
+      const icon = markers[i][4]
       marker = new google.maps.Marker({
         title: title,
         position: position,
         icon: icon,
-        url: url,
-      });
+        url: url
+      })
 
-      google.maps.event.addListener(marker, "click", function () {
-        window.open(this.url, "_blank");
-      });
+      google.maps.event.addListener(marker, 'click', function () {
+        window.open(this.url, '_blank')
+      })
 
-      bounds.extend(position);
-      marker.setMap(map);
+      bounds.extend(position)
+      marker.setMap(map)
     }
 
     if (markers.length > 1) {
-      map.fitBounds(bounds);
+      map.fitBounds(bounds)
     }
   }
 }
 
 (function () {
-  "use strict";
+  'use strict'
 
-  const InitMaps = function () {
-    const ctns = document.querySelectorAll('[id^="map-"]');
-    if (!!ctns[0]) {
-      let key = ctns[0].dataset.key;
-      let script = document.createElement("script");
+  const addMapScript = function () {
+    const ctns = document.querySelectorAll('[id^="map-"]')
+    if (ctns[0]) {
+      const key = ctns[0].dataset.key
+      const script = document.createElement("script")
       script.src =
-        "https://maps.googleapis.com/maps/api/js?key=" +
+        'https://maps.googleapis.com/maps/api/js?key=' +
         key +
-        "&callback=initMap";
-      script.defer = true;
-      script.async = true;
-      document.body.appendChild(script);
+        '&callback=initMap'
+      script.defer = true
+      script.async = true
+      document.body.appendChild(script)
     }
-  };
+  }
 
-  InitMaps();
-})();
+  addMapScript()
+})()
