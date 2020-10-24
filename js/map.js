394584b48671ad79ad2fba910c6b6cda4e9e9cb7
hotdoy <hotdoy@users.noreply.github.com>
2020-10-24 15:01:59 +0000

    Prettified Code!

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
-function initMap () {
-  const ctns = document.querySelectorAll('[id^="map-"]')
+function initMap() {
+  const ctns = document.querySelectorAll('[id^="map-"]');
   for (let ctn of ctns) {
-    const zoom = ctn.dataset.zoom ? parseInt(ctn.dataset.zoom) : ''
-    const markers = ctn.dataset.markers ? JSON.parse(ctn.dataset.markers) : ''
-    const styles = ctn.dataset.styles ? JSON.parse(ctn.dataset.styles) : ''
-    let marker, i
-    const bounds = new google.maps.LatLngBounds()
+    const zoom = ctn.dataset.zoom ? parseInt(ctn.dataset.zoom) : "";
+    const markers = ctn.dataset.markers ? JSON.parse(ctn.dataset.markers) : "";
+    const styles = ctn.dataset.styles ? JSON.parse(ctn.dataset.styles) : "";
+    let marker, i;
+    const bounds = new google.maps.LatLngBounds();
 
     const map = new google.maps.Map(ctn, {
       zoom: zoom,
@@ -16,53 +16,53 @@ function initMap () {
       streetViewControl: false,
       mapTypeControl: false,
       scrollwheel: false,
-      mapTypeId: 'roadmap',
-      draggable: true
-    })
+      mapTypeId: "roadmap",
+      draggable: true,
+    });
 
     for (i = 0; i < markers.length; i++) {
-      const title = markers[i][0]
-      const position = new google.maps.LatLng(markers[i][1], markers[i][2])
-      const url = markers[i][3]
-      const icon = markers[i][4]
+      const title = markers[i][0];
+      const position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+      const url = markers[i][3];
+      const icon = markers[i][4];
       marker = new google.maps.Marker({
         title: title,
         position: position,
         icon: icon,
-        url: url
-      })
+        url: url,
+      });
 
-      google.maps.event.addListener(marker, 'click', function () {
-        window.open(this.url, '_blank')
-      })
+      google.maps.event.addListener(marker, "click", function () {
+        window.open(this.url, "_blank");
+      });
 
-      bounds.extend(position)
-      marker.setMap(map)
+      bounds.extend(position);
+      marker.setMap(map);
     }
 
     if (markers.length > 1) {
-      map.fitBounds(bounds)
+      map.fitBounds(bounds);
     }
   }
 }
 
 (function () {
-  'use strict'
+  "use strict";
 
   const addMapScript = function () {
-    const ctns = document.querySelectorAll('[id^="map-"]')
+    const ctns = document.querySelectorAll('[id^="map-"]');
     if (ctns[0]) {
-      const key = ctns[0].dataset.key
-      const script = document.createElement("script")
+      const key = ctns[0].dataset.key;
+      const script = document.createElement("script");
       script.src =
-        'https://maps.googleapis.com/maps/api/js?key=' +
+        "https://maps.googleapis.com/maps/api/js?key=" +
         key +
-        '&callback=initMap'
-      script.defer = true
-      script.async = true
-      document.body.appendChild(script)
+        "&callback=initMap";
+      script.defer = true;
+      script.async = true;
+      document.body.appendChild(script);
     }
-  }
+  };
 
-  addMapScript()
-})()
+  addMapScript();
+})();
