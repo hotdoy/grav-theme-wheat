c97e560079f61588aabde7e5c519e265a154d756
Vincent <rouleau.vincent@gmail.com>
2021-02-16 07:40:36 -0500

    fixed fx

--- js/fx.js
+++ js/fx.js
@@ -2,23 +2,12 @@ const FX = {
 
     Module: function() {
         ScrollOut({
-            targets: '.module',
+            // targets: '.module',
             once: true,
-        });
-    },
-
-    Marquee: function() {
-        ScrollOut({
-            targets: '.module .marquee',
-            cssProps: {
-                viewportY: true,
-                visibleY: true
-            }
         });
     },
 
     Init: function() {
         FX.Module(); 
-        FX.Marquee(); 
     },
 };
