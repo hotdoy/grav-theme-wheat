31220077521e00c8f909860c46bdd3c3803cfb6c
Vincent <rouleau.vincent@gmail.com>
2021-04-01 19:19:50 -0400

    WIP variant selector

--- js/product.js
+++ js/product.js
@@ -1,15 +1,24 @@
-(function () {
-  "use strict";
+(function() {
+    "use strict";
 
-  const initProduct = function () {
-    // Swiper
-    var swiper = new Swiper(".product__showcase .swiper-container", {
-      centeredSlides: true,
-      pagination: {
-        el: ".swiper-pagination",
-      },
-    });
-  };
+    const initProduct = function() {
 
-  initProduct();
+        // Swiper
+        const swiper = new Swiper(".product .product__showcase .swiper-container", {
+            centeredSlides: true,
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        });
+
+        // Variant
+        const selects = document.querySelectorAll(".product .product__showcase .variant select");
+        for (let select of selects) {
+            select.addEventListener('change', function(){
+              console.log(select.options[select.selectedIndex].value);
+            });
+        };
+    };
+
+    initProduct();
 })();
