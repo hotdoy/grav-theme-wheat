d59bf203613fd0e335705c29fbace83722de362a
Vincent <rouleau.vincent@gmail.com>
2021-04-22 08:24:13 -0400

    dialog history stuff WIP

--- js/dialog.js
+++ js/dialog.js
@@ -1,28 +1,50 @@
 const Dialog = {
 
+    e: null,
+    hash:'',
+    resetDelay: 200,
+
     watch: function() {
         window.addEventListener('hashchange', function() {
             if (!!document.querySelectorAll(window.location.hash + ' .dialog')) {
-                Dialog.toggle(window.location.hash.substring(1));
+                Dialog.e = document.getElementById(window.location.hash.substring(1));
+                Dialog.toggle();
+                Dialog.watchOutsideClick();
             }
         });
     },
 
-    toggle: function(id) {
-        const e = document.getElementById(id);
-        if (!!e) {
-            if (!e.classList.contains('dialog--active')) {
-                e.classList.add('dialog--active');
+    toggle: function() {
+        if (!!Dialog.e) {
+            if (!Dialog.e.classList.contains('dialog--active')) {
+                Dialog.e.classList.add('dialog--active');
             } else {
-                e.classList.remove('dialog--active');
+                Dialog.e.classList.remove('dialog--active');
+                Dialog.reset();
             }
         }
     },
 
-    watchOutsideClick: function(){},
-    watchEscapeKey: function(){},
-    watchHistoryBack: function(){},
+    watchOutsideClick: function() {
+        if (!!Dialog.e) {
+            Dialog.e.querySelectorAll('.dialog__veil')[0].addEventListener('click', function() {
+                Dialog.toggle();
+            })
+        }
+    },
 
+    // watchEscapeKey: function() {},
+
+    reset: function() {
+        if (!!Dialog.e) {
+            setTimeout(function() {
+                const clone = Dialog.e.cloneNode(true);
+                Dialog.e.parentNode.replaceChild(clone, Dialog.e);
+                Dialog.e = null;
+                history.pushState("", document.title, window.location.pathname + window.location.search);
+            }, Dialog.resetDelay);
+        }
+    },
 };
 
 Dialog.watch();
