ac123b39054fe98a83e73982809bf74846e503dc
Vincent <rouleau.vincent@gmail.com>
2021-04-25 16:59:57 -0400

    WIP dialog

--- js/dialog.js
+++ js/dialog.js
@@ -5,29 +5,63 @@ const Dialog = {
     open: function(id) {
         const e = document.getElementById(id);
         e.classList.add('dialog--active');
-        Dialog.closeOnOutside(e);
+        Dialog.closeOnOutsideClick(id);
     },
 
     close: function(id) {
         const e = document.getElementById(id);
         e.classList.remove('dialog--active');
-        Dialog.reset(e);
+        Dialog.reset(id);
     },
 
-    closeOnOutside: function(e) {
-        if (!!e) {
-            e.querySelectorAll('.dialog__veil')[0].addEventListener('click', function() {
-                Dialog.close(e.id);
+    closeAll: function() {
+        const el = document.querySelectorAll('.dialog');
+        for (let e of el) {
+            Dialog.close(e.id);
+        }
+    },
+
+    // THIS DOES NOT WORK
+    closeOnOutsideClick: function(id) {
+        const e = document.getElementById(id);
+        const veil = e.querySelectorAll('.dialog__veil');
+        console.log('1');
+        for(let v of veil) {
+            console.log('2');
+            v.addEventListener('click', function(event) {
+                console.log('3');
+                Dialog.close(id);
             })
         }
     },
 
-    reset: function(e) {
+    reset: function(id) {
+        const e = document.getElementById(id);
         if (!!e) {
             setTimeout(function() {
                 const clone = e.cloneNode(true);
                 e.parentNode.replaceChild(clone, e);
             }, Dialog.resetDelay);
         }
-    },    
+    },
+
+    watch: function() {
+        window.addEventListener('hashchange', function(event) {
+            Dialog.init();
+        })
+    },
+
+    init: function() {
+        Dialog.closeAll();
+        const hash = window.location.hash;
+        if (!!hash) {
+            const el = document.querySelectorAll('.dialog' + hash);
+            for (let e of el) {
+                Dialog.open(e.id); 
+            }              
+        }
+        Dialog.watch();
+    },
 };
+
+Dialog.init();
