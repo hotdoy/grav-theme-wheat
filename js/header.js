bb779830ef8c53276c0ffbe2c4f22fa2d6382ce0
Vincent <rouleau.vincent@gmail.com>
2021-06-21 08:18:33 -0400

    WIP refactoring header

--- js/header.js
+++ js/header.js
@@ -1,26 +1,27 @@
-(function() {
-    "use strict";
+const Header = {
+    els: document.querySelectorAll("#header .burger"),
+    b: document.body.classList,
 
-    const InitHeader = function() {
-        const toggles = document.querySelectorAll("#header .grid .burger");
-        let lastOffset = 0;
-
-        for (let toggle of toggles) {
-            toggle.addEventListener("click", function() {
-                if (document.body.classList.contains('header-panel--open')) {
-                    document.body.classList.remove("header-panel--open");
+    Init: function() {
+        Header.els.forEach((el) => {
+            el.addEventListener("click", function() {
+                if (Header.b.contains('header-panel--open')) {
+                    Header.b.remove("header-panel--open");
                 } else {
-                    document.body.classList.add("header-panel--open");
+                    Header.b.add("header-panel--open");
                 } 
             });
-        }
+        });
+    },
 
+    Watch: function() {
         window.addEventListener("resize", function() {
-            if (window.innerWidth > 735 && document.body.classList.contains("header-panel--open")) {
-                document.body.classList.remove("header-panel--open");
+            if (window.innerWidth > 735 && Header.b.contains("header-panel--open")) {
+                Header.b.remove("header-panel--open");
             }
         });
-    };
+    },
+};
 
-    InitHeader();
-})();
+Header.Init();
+Header.Watch();
