76028755117c0451677503dbfcf2e3a9a4129aac
Vincent <rouleau.vincent@gmail.com>
2021-07-01 06:47:35 -0400

    organized css to mirror templates

--- js/page-state.js
+++ js/page-state.js
@@ -1,52 +1,96 @@
 const PageState = {
     state: 'loading',
-    interactiveDelay: 0,
-    navigationDelay: 200,
+    href: window.location.href,
+    hostname: window.location.hostname,
+    path: window.location.pathname,
+    destination: '',
+    currentDepth: '',
+    destinationDepth: 0,
+    intDelay: 0,
+    navDelay: 200,
     fxDelay: 0,
     t: performance.now(),
 
     Log: function(message) {
-        console.log('%c page-state: ' + PageState.state + ' - ' + PageState.t, 'color:green;');
+        console.log('%c' + this.state + ' @ ' + this.t, 'color:green;');
     },
 
-    GetAdjustDelay: function(delay) {
+    GetDelay: function(delay) {
         if (delay <= 0) {
             return delay;
         } else {
-            return delay - PageState.t;             
+            return delay - this.t;             
         }
     },
 
-    UpdateState: function(state) {
+    GetInteractiveDelay: function() {
+        return this.GetDelay(this.intDelay);
+    },
 
-        switch(PageState.state) {
+    GetFxDelay: function() {
+        return this.GetDelay(this.FxDelay);
+    },
+
+    GetDirection: function() {},
+
+    GetDepth: function(url) {
+        if (url.length) {
+            return url.split('/').length - 1;
+        } else {
+            return 0;
+        }
+    },
+
+    SetDestinationDepth: function() {
+        this.destinationDepth = this.GetDepth(this.destination);
+    },
+
+    SetCurrentDepth: function() {
+        this.currentDepth = this.GetDepth(this.path);
+    },
+
+    UpdateStateAttr: function() {
+        document.body.setAttribute('data-page-state', this.state);
+    },
+
+    UpdateState: function() {
+
+        this.SetCurrentDepth();
+
+        switch(this.state) {
 
             // INTERACTIVE
             case 'loading':
-                PageState.state = 'interactive';
-                PageState.t = performance.now();
+                this.state = 'interactive';
+                this.t = performance.now();
+
                 setTimeout(function(){ 
                     FX.Init();
-                }, PageState.GetAdjustDelay(PageState.fxDelay)); 
-                PageState.UpdateStateAttr(PageState.GetAdjustDelay(PageState.interactiveDelay));
+                }, this.GetFxDelay());
+                
+                setTimeout(function(){ 
+                    PageState.UpdateStateAttr();
+                }, this.GetInteractiveDelay());
+
                 break;
 
             // NAVIGATING
             case 'interactive':
-                PageState.state = 'navigating';
-                PageState.UpdateStateAttr();
+                if (this.destinationDepth < this.currentDepth) {
+                    this.state = 'navigating-backward';
+                } else if(this.destinationDepth > this.currentDepth) {
+                    this.state = 'navigating-forward';   
+                } else {
+                    this.state = 'navigating';   
+                }
                 Dialog.closeAll();
+                this.UpdateStateAttr();
                 break;
         }
 
-        PageState.Log();
+        this.Log();
     },
 
-    UpdateStateAttr: function(delay) {
-        setTimeout(function(){ 
-            document.body.setAttribute('data-page-state', PageState.state);
-        }, delay);
-    },
 };
 
 PageState.UpdateState(); 
