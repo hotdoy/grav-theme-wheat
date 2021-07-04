5785cd4fc5ba921eec8004d09fc3ab52b0ea1a18
Vincent <rouleau.vincent@gmail.com>
2021-07-04 15:01:32 -0400

    added mutation observer and support images loaded before script

--- js/page-state/img.js
+++ js/page-state/img.js
@@ -1,14 +1,24 @@
 const Img = {
+    Watch: function() {
+        const target = document.querySelector("#main");
+        const config = { childList: true, subtree: true };
+        const observer = new MutationObserver(function() {
+            Img.Init();
+        });
+        observer.observe(target, config);
+    },
 
     SetAttr: function(el, state) {
         el.setAttribute('data-loading-state', state);
     },
 
     Init: function() {
+        this.Watch();
         document.querySelectorAll('img', '[loading="lazy"]').forEach(el => {
-            el.onload = function () {
-                Img.SetAttr(el, 'complete')
-            };
+            el.onload = () =>  Img.SetAttr(el, 'complete');
+            if (el.complete) {
+                Img.SetAttr(el, 'complete');
+            }
         });
     },
 };
