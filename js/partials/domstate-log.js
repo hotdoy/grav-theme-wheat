8ff0a09b1bd3995a680c20ee28f88468df2b4dca
Vincent <rouleau.vincent@gmail.com>
2021-08-20 07:27:36 -0400

    v0.16.2

--- js/partials/domstate-log.js
+++ js/partials/domstate-log.js
@@ -1,7 +1,6 @@
 document.addEventListener('domstateComplete', () => {
 
     const ttc = (performance.now()/1000).toPrecision(2);
-    console.log(ttc + 's');
     document.querySelectorAll('.timeto-complete').forEach(el => {
         el.innerHTML = ttc + 's';
     });
