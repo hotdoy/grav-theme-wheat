c8a01e54cdc72d2a8947968dad0a7764e03603c2
Vincent <rouleau.vincent@gmail.com>
2021-10-27 17:25:09 -0400

    a lot

--- js/partials/app.js
+++ js/partials/app.js
@@ -1,48 +1,20 @@
 const App = {
     b: document.body,
-    path: window.location.pathname,
-    depth: 0,
-    dest: '',
-    destDepth: 0,
+    currentPath: window.location.pathname,
+    currentDepth: 0,
+    destinatioPath: '',
+    destinationDepth: 0,
     delay: 0,
-    navDelay: 0,
+    navigationDelay: 0,
     perceivedDelay: performance.now(),
     adjustedDelay: 0,
     events: {
-        ready: new Event('appReady'),
-        update: new Event('appUpdate'),
+        // update: new Event('appUpdate'),
         interactive: new Event('appInteractive'),
         complete: new Event('appComplete'),
         navigating: new Event('appNavigating'),
         navigatingForward: new Event('appNavigatingForward'),
         navigatingBackward: new Event('appNavigatingBackward'),
-    },
-
-    getAdjustedDelay: function(delay) {
-        if (delay <= 0) {
-            return delay;
-        } else {
-            return delay - this.perceivedDelay;             
-        }
-    },
-
-    getDepth: function(url) {
-        if (url && url.length) {
-            return url.split('/').length - 1;
-        } else {
-            return 0;
-        }
-    },
-
-    observeMutation: function(selector, callback) {
-        const target = document.querySelector(selector);
-        const config = { attributes: false, childList: true, subtree: true };
-        const observer = new MutationObserver(function() {
-            callback();
-            observer.disconnect();
-        });
-        observer.observe(target, config);
-        return;
     },
 
     setState: function(el, state, delay, events) {
@@ -56,76 +28,134 @@ const App = {
         }, delay);
     },
 
-    setExtLinkAttr: function(el) {
+    getAdjustedDelay: function(delay) {
+        if (delay <= 0) {
+            return delay;
+        } else {
+            return delay - this.perceivedDelay;             
+        }
+    },
+
+    getDepth: function(path) {
+        if (path && path.length) {
+            return path.split('/').length - 1;
+        } else {
+            return 0;
+        }
+    },
+
+    setCurrentDepth: function(depth) {
+        if (depth) {
+            App.currentDepth = depth;
+        } else {
+            App.currentDepth = App.getDepth(App.currentPath);
+        }
+    },
+
+    setDestinationPath: function(path) {
+        if (path.length) {
+            App.destinatioPath = path;  
+        }
+    },
+
+    setDestinationDepth: function(depth) {
+        if (depth) {
+            App.destinationDepth = depth;
+        } else {
+            App.destinationDepth = App.getDepth(App.destinatioPath);
+        }
+    },
+
+    setAdjustedDelay: function() {
+        App.adjustedDelay = App.getAdjustedDelay(App.delay);
+    },
+
+    observeMutation: function(selector, callback) {
+        const target = document.querySelector(selector);
+        const config = { attributes: false, childList: true, subtree: true };
+        const observer = new MutationObserver(function() {
+            callback();
+            observer.disconnect();
+        });
+        observer.observe(target, config);
+        return;
+    },
+
+    setExtLinkBehaviour: function(el) {
         el.setAttribute("target", "_blank");
         el.setAttribute("rel", "noopener");
     },
 
+    setIntLinkBehaviour: function(el) {
+        el.addEventListener("click", function(e) {
+            e.preventDefault();
+            const destinationPath = el.getAttribute("href");
+            App.setDestinationPath(destinationPath);
+            App.setDestinationDepth()         
+            App.setPrerenderLink(); 
+
+            if (App.destinationDepth < App.currentDepth) {
+                App.setState(App.b, 'navigating-backward', 0, [App.events.navigating, App.events.navigatingBackward]);
+
+            } else if(App.destinationDepth > App.currentDepth) {
+                App.setState(App.b, 'navigating-forward', 0, [App.events.navigating, App.events.navigatingForward]);
+
+            } else {
+                App.setState(App.b,'navigating', 0, [App.events.navigating]);
+            }
+            setTimeout(function(){ 
+                window.location.href = App.destinatioPath;
+            }, App.navigationDelay);
+
+        }, false);
+    },
+
     setPrerenderLink: function() {
-        if (App.navDelay > 0) {
-            const l = document.createElement('link');
-            l.href = App.dest;
-            l.rel='prerender';
-            document.getElementsByTagName('head')[0].appendChild(l);    
+        if (App.navigationDelay > 0) {
+            const link = document.createElement('link');
+            link.href = App.destinatioPath;
+            link.rel='prerender';
+            document.getElementsByTagName('head')[0].appendChild(link);    
         } 
     },
 
-    navigation: function(els) {
+    setLinkBehaviour: function(els) {
         els.forEach(el => {
-
-            const href = el.getAttribute("href");
-            
-            if (!!href && href.match("^http")) {
-                App.setExtLinkAttr(el);
-            } else if (!!href && href.match("^/")) {
-                el.addEventListener("click", function(e) {
-                    e.preventDefault();
-
-                    App.dest = href;                    
-                    App.destDepth = App.getDepth(App.dest);
-                    App.setPrerenderLink(); 
-
-                    if (App.destDepth < App.depth) {
-                        App.setState(App.b, 'navigating-backward', 0, [App.events.navigating, App.events.navigatingBackward]);
-                    } else if(App.destDepth > App.depth) {
-                        App.setState(App.b, 'navigating-forward', 0, [App.events.navigating, App.events.navigatingForward]);
-                    } else {
-                        App.setState(App.b, 'navigating', 0, [App.events.navigating]);
-                    }
-
-                    setTimeout(function(){ 
-                        window.location.href = App.dest;
-                    }, App.navDelay);
-
-                }, false);
+            const url = el.getAttribute("href");
+            if (!!url && url.match("^http")) {
+                App.setExtLinkBehaviour(el);
+            } 
+            if (!!url && url.match("^/")) {
+                App.setIntLinkBehaviour(el);
             }
         });
     },
 
+    setImgBehaviour: function(els) {
+        els.forEach(el => {
+            el.onload = function() {
+                App.setState(el, 'complete');
+                // el.removeAttribute('style');
+            };
+            if (el.complete) {
+                App.setState(el, 'complete');
+                // el.removeAttribute('style');
+            };
+        });
+    },
+
     Init: function() {
-        App.depth = App.getDepth(App.path);
-        App.adjustedDelay = App.getAdjustedDelay(App.delay);
+        App.setCurrentDepth();
+        App.setAdjustedDelay();
+        App.setLinkBehaviour(document.querySelectorAll("a"));
+        App.setImgBehaviour(document.querySelectorAll('img'));
+
         document.addEventListener('readystatechange', e => {
             if (e.target.readyState === 'complete') {
                 App.setState(App.b, 'complete', App.adjustedDelay, [App.events.complete]);
             }
         });
 
-        App.navigation(document.querySelectorAll("a"));
-
-        // IMG
-        document.querySelectorAll('img').forEach(el => {
-            el.onload = function() {
-                App.setState(el, 'complete');
-                el.removeAttribute('style');
-            };
-            if (el.complete) {
-                App.setState(el, 'complete');
-                el.removeAttribute('style');
-            };
-        });
-
-        document.dispatchEvent(App.events.ready);
         App.setState(App.b, 'interactive', App.adjustedDelay, [App.events.interactive]);
     }
 };
