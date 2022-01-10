069a5ecd8d481527e4abf79c1680bc4c091c02bd
Vincent <rouleau.vincent@gmail.com>
2022-01-10 08:24:05 -0500

    wip share

--- js/partials/share.js
+++ js/partials/share.js
@@ -1,79 +1,53 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/share
 (function () {
-  "use strict";
+	'use strict';
 
-  const InitShare = function () {
-    const shareButtons = document.querySelectorAll("[data-share-button]");
-    const shareDialog = document.getElementById("share__dialog");
-    const closeDialogButton = shareDialog.getElementsByClassName(
-      "share__dialog__close"
-    );
-    const title = document.title;
-    const url = window.location.href;
-    const targets = shareDialog.getElementsByClassName("js-share-target");
+  const el = document.querySelector('.share')
+	const copyBtn = el.querySelector(sels.copyBtn);
+	const shareBtn = el.querySelector(sels.button);
+	const copyResult = el.querySelector(sels.copyResult);
 
-    // Share Buttons
-    for (let shareButton of shareButtons) {
-      shareButton.addEventListener("click", (event) => {
-        if (navigator.share) {
-          navigator.share({
-            title: title,
-            url: url,
-          });
-          console.log("Thanks for sharing!");
-        } else {
-          console.log("Share API not supported!");
-          shareDialog.classList.add("share__dialog--on");
-        }
-      });
-    }
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(location.href).then(
+			function () {
+				copyResult.innerHTML = 'Link copied!';
+				copyResult.classList.add('display');
+			},
+			function () {
+				copyResult.innerHTML = 'Copy failed.';
+				copyResult.classList.add('display');
+			}
+		);
+		setTimeout(function () {
+			copyResult.classList.remove('display');
+		}, 2000);
+	};
 
-    // Close button
-    for (let btn of closeDialogButton) {
-      btn.addEventListener("click", (event) => {
-        if (shareDialog.classList.contains("share__dialog--on")) {
-          shareDialog.classList.remove("share__dialog--on");
-        }
-        event.stopImmediatePropagation;
-      });
-    }
+	const initShare = () => {
+		displayShare();
+		if (shareBtn) {
+			shareBtn.addEventListener('click', () => {
+				navigator
+					.share({
+						title: document.title,
+						url: location.href
+					})
+					.catch(() => {
+						return;
+					});
+			});
+		}
 
-    // Close by outside click
-    shareDialog.addEventListener("click", (event) => {
-      if (
-        event.target == shareDialog &&
-        shareDialog.classList.contains("share__dialog--on")
-      ) {
-        shareDialog.classList.remove("share__dialog--on");
-      }
-    });
+		if (copyBtn) {
+			copyBtn.addEventListener('click', () => {
+				navigator.permissions.query({ name: 'clipboard-write' }).then((result) => {
+					if (result.state == 'granted' || result.state == 'prompt') {
+						copyToClipboard();
+					}
+				});
+			});
+		}
+	};
 
-    for (let target of targets) {
-      if (target.classList.contains("facebook")) {
-        target.addEventListener("click", (event) => {
-          window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`);
-        });
-      } else if (target.classList.contains("twitter")) {
-        target.addEventListener("click", (event) => {
-          window.open(
-            `https://twitter.com/intent/tweet?url=${url}&title=${title}`
-          );
-        });
-      } else if (target.classList.contains("linkedin")) {
-        target.addEventListener("click", (event) => {
-          window.open(
-            `https://www.linkedin.com/sharing/share-offsite/?url=${url}`
-          );
-        });
-      } else if (target.classList.contains("reddit")) {
-        target.addEventListener("click", (event) => {
-          window.open(`https://reddit.com/submit?url=${url}&title=${title}`);
-        });
-      } else if (target.classList.contains("email")) {
-        target.addEventListener("click", (event) => {
-          window.open(`mailto:?subject=${title}&body=${url}`);
-        });
-      }
-    }
-  };
-  InitShare();
+	initShare();
 })();
