0a5c16f86d4db63139996c2d4d785ddb07f51bd4
Calotte <rouleau.vincent@gmail.com>
2022-01-10 20:26:48 -0500

    improved share

--- js/partials/share.js
+++ js/partials/share.js
@@ -1,24 +1,30 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/share
 (function () {
 	'use strict';
-	const share = document.getElementById('share');
-	const nativeShare = share.querySelector('.native');
-	const shareData = {
-		title: document.title,
-		text: document.querySelector('meta[name="description"]').content,
-		url: location.href
-	  }
+	const initShare = function() {
+		const share = document.getElementById('share');
+		if (share) {
+			const system = share.querySelector('.system');
+			const fallback = share.querySelector('.fallback');
+			const shareData = {
+				title: document.title,
+				text: document.querySelector('meta[name="description"]').content,
+				url: location.href
+			};
 
-	const initShare = () => {
-		nativeShare.addEventListener('click', async () => {
-			try {
-				await navigator.share(shareData);
-				console.log('Shared!');
-			} catch(err) {
-				console.log('Error while sharing.');
+			if (navigator.canShare && location.protocol === 'https:') {
+				fallback.remove();
+				system.querySelector('a').addEventListener('click', async () => {
+					try {
+						await navigator.share(shareData);
+						console.log('Shared!');
+					} catch(err) {
+						console.log('Error while sharing.');
+					}
+				});				
+			} else {
+				system.remove();
 			}
-		});			
+		}		
 	};
-
-	// initShare();
+	initShare();
 })();
