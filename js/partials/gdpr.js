46dec994bb040c817cb0c83766bec7fb9f3ede89
Calotte <rouleau.vincent@gmail.com>
2022-02-11 15:52:19 -0500

    gdpr stuff

---
+++ js/partials/gdpr.js
@@ -0,0 +1,18 @@
+(function() {
+    "use strict";
+
+    const initGdpr = function() {
+        const e = document.getElementById('gdpr');
+        const b = e.querySelector('button');
+        
+        if (!localStorage.getItem('accept_cookies')) {
+            e.style.display = 'flex';
+            b.addEventListener('click', function() {
+                localStorage.setItem('accept_cookies', 'true');
+                e.remove();
+            });
+        }
+
+    };
+    initGdpr();
+})();
