32965f4301af72e979be4581a100c416e387962d
Calotte <rouleau.vincent@gmail.com>
2022-04-23 11:44:42 -0400

    a lot, whatever

--- js/partials/sticky.js
+++
@@ -1,8 +0,0 @@
-const observer = new IntersectionObserver( 
-  ([e]) => e.target.classList.toggle("stuck", e.intersectionRatio < 1),
-  { threshold: [1] }
-);
-
-document.querySelectorAll(".sticky").forEach(el => {
-    observer.observe(el);    
-});
