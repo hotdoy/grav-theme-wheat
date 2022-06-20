d41680fb3fc6fc33fb4d18d35cd11520fdbcf110
Vincent <rouleau.vincent@gmail.com>
2022-06-20 14:25:37 -0400

    added fathom exclusion

---
+++ js/partials/fathom.js
@@ -0,0 +1,14 @@
+(function () {
+	"use strict";
+    window.addEventListener('load', (event) => {
+        const qs = new URLSearchParams(window.location.search);
+        const blockTrackingForMe = qs.get('blockTrackingForMe');
+        const enableTrackingForMe = qs.get('enableTrackingForMe');
+        if (!!blockTrackingForMe) {
+            fathom.blockTrackingForMe();
+        }
+        if (!!enableTrackingForMe) {
+            fathom.enableTrackingForMe();
+        }
+    });
+})();
