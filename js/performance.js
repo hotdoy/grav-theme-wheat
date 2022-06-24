78512122221d377debebc0d5c6686bab5d398b09
Vincent <rouleau.vincent@gmail.com>
2022-06-24 14:24:39 -0400

    adjusted carbon calculation for new test round

--- js/performance.js
+++ js/performance.js
@@ -3,14 +3,35 @@
     let tti = 0;
     let ttc = 0;
     let t = 0;
+
     document.addEventListener('readystatechange', (event) => {
         t = performance.now();
         let state = event.target.readyState;
         if (state == 'interactive') {
             tti = (t/1000).toPrecision(2);
         } else if (state == 'complete') {
+
+            // TIME TO COMPLETE
             ttc = (t/1000).toPrecision(2);
-            console.log(`⚡This page became interactive in ${tti}s and complete ${ttc}s`);
+            console.log(`⚡This site became interactive in ${tti}s and complete in ${ttc}s`);
+
+            // TRANFER
+            let transferInB = 0;
+            const rs = performance.getEntriesByType('resource');
+            rs.forEach(r => {
+                transferInB = transferInB + r['transferSize'];
+            });
+
+            // CO2 MATHS
+            const transferInKb = transferInB / 1000;
+            const transferInGb = (transferInB / 1e+9);
+            const kwhPerGb = 0.81;
+            const carbonFactor = 50; // 442 for bog standard | 50 for renewable
+            const kwhPerVisit = transferInGb * kwhPerGb;
+            const co2PerVisit = kwhPerVisit * carbonFactor;
+            console.log(`🌲Transfered around ${transferInKb.toPrecision(5)} KB in ${rs.length} requests for an estimate of ${co2PerVisit.toPrecision(2)}g of CO2.`);
+           
+            // FRONT-END
             document.querySelectorAll('.ttc').forEach(el => {
                 el.innerHTML = ttc + 's';
             });
@@ -19,18 +40,12 @@
             });
         }
     });
-})();
-
-(function () {
-	"use strict";
     htmx.onLoad(function(){
-        let t = 0;
-        const rs = performance.getEntriesByType('resource');
-        rs.forEach(r => {
-            t = t + r['transferSize']
+        document.querySelectorAll('.ttc').forEach(el => {
+            el.innerHTML = ttc + 's';
         });
-        // console.log(`🌲${t/1000} KB in ${rs.length} requests. That's about ${((t*1e-9)*3).toPrecision(4)} KG of CO2.`);
-        console.log(`🌲${t/1000} KB in ${rs.length} requests.`);
-    })
+        document.querySelectorAll('.tti').forEach(el => {
+            el.innerHTML = tti + 's';
+        });
+    });
 })();
-
