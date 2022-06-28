3cbbdfcc40bbc05462b8c4a6437f9072e75e7f2d
Vincent <rouleau.vincent@gmail.com>
2022-06-28 11:52:47 -0400

    fixed defered js

--- js/link.js
+++ js/link.js
@@ -25,33 +25,13 @@
     };
 
     const SetIntLinkBehaviour = function(el) {
-        // el.addEventListener("click", function(e) {
-        //     e.preventDefault();
-        //     const destinationPath = el.getAttribute("href");
-        //     App.setDestinationPath(destinationPath);
-        //     App.setDestinationDepth()         
-
-        //     if (App.destinationDepth < App.currentDepth) {
-        //         App.setState(App.body, '_012_nav-b', 0, [App.events.navigating, App.events.navigatingBackward]);
-
-        //     } else if(App.destinationDepth > App.currentDepth) {
-        //         App.setState(App.body, '_012_nav-f', 0, [App.events.navigating, App.events.navigatingForward]);
-
-        //     } else {
-        //         App.setState(App.body,'_012_nav', 0, [App.events.navigating]);
-        //     }
-        //     setTimeout(function(){ 
-        //         window.location.href = App.destinatioPath;
-        //     }, App.navigationDelay);
-
-        // }, false);
+        // DO SOMETHING WITH INTERNAL LINKS
     };
 
     const ObserveMutation = function () {
         let o = new MutationObserver(SetLinkBehaviour);
         o.observe(document.body, {childList: true, subtree: true});
     };
-
     SetLinkBehaviour();
     ObserveMutation();
 })();
--- js/scroll.js
+++ js/scroll.js
@@ -1,27 +1,27 @@
 (function () {
 	"use strict";
     const b = document.body;
-  	let lastScroll = 0;
+    let lastScroll = 0;
     if (window.scrollY < 20) {
         b.classList.add('top');
     }
-	window.addEventListener("scroll", () => {
-		const currentScroll = window.pageYOffset;
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset;
         // top
         if (window.scrollY < 20) {
             b.classList.add('top');
-		} else {
+        } else {
             b.classList.remove('top');
         }
         // down
-         if (currentScroll > lastScroll + 10) {
+        if (currentScroll > lastScroll + 10) {
             b.classList.remove('up');
-			b.classList.add('down');
-		// up
-		} else if (currentScroll < lastScroll - 10) {
+            b.classList.add('down');
+        // up
+        } else if (currentScroll < lastScroll - 10) {
             b.classList.remove('down');
-			b.classList.add('up');
-		}
-    	lastScroll = currentScroll;
-  	});
+            b.classList.add('up');
+        }
+        lastScroll = currentScroll;
+    });
 })();
