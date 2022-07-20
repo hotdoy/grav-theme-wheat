9e43e2db0d3ad7cc42b0cd13e9f3b2a16bef29ba
Vincent <rouleau.vincent@gmail.com>
2022-07-19 20:21:54 -0400

    prettyfied stuff

--- js/colorscheme.js
+++ js/colorscheme.js
@@ -1,18 +1,18 @@
 (function () {
 	"use strict";
-    const a = window.matchMedia('(prefers-color-scheme: dark)');
-    if (a.matches) {
-        document.body.classList.add('colorscheme-dark');
-    }
-    a.addEventListener('change', event => {
-        if (event.matches) {
-            document.body.classList.add('colorscheme-dark');
-        } else {
-            document.body.classList.remove('colorscheme-dark');
-        }
-    })
+	const a = window.matchMedia("(prefers-color-scheme: dark)");
+	if (a.matches) {
+		document.body.classList.add("colorscheme-dark");
+	}
+	a.addEventListener("change", (event) => {
+		if (event.matches) {
+			document.body.classList.add("colorscheme-dark");
+		} else {
+			document.body.classList.remove("colorscheme-dark");
+		}
+	});
 })();
 
 const ToggleColorScheme = function () {
-    document.body.classList.toggle('colorscheme-dark');
-}
+	document.body.classList.toggle("colorscheme-dark");
+};
--- js/img.js
+++ js/img.js
@@ -1,19 +1,19 @@
 (function () {
 	"use strict";
-    const SetImgBehavior = function () {
-        document.querySelectorAll('img').forEach(el => {
-            el.onload = function() {
-                el.setAttribute('loaded', '');
-            };
-            if (el.complete) {
-                el.setAttribute('loaded', '');
-            };
-        });
-    };
-    const ObserveMutation = function () {
-        let o = new MutationObserver(SetImgBehavior);
-        o.observe(document.body, {childList: true, subtree: true});
-    };
-    SetImgBehavior();
-    ObserveMutation();
+	const SetImgBehavior = function () {
+		document.querySelectorAll("img").forEach((el) => {
+			el.onload = function () {
+				el.setAttribute("loaded", "");
+			};
+			if (el.complete) {
+				el.setAttribute("loaded", "");
+			}
+		});
+	};
+	const ObserveMutation = function () {
+		let o = new MutationObserver(SetImgBehavior);
+		o.observe(document.body, { childList: true, subtree: true });
+	};
+	SetImgBehavior();
+	ObserveMutation();
 })();
