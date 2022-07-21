36dfbc981b3297d7d07e35d690f646e82eede6a2
Vincent <rouleau.vincent@gmail.com>
2022-07-21 14:58:26 -0400

    leave basic loader structure in code

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,6 @@
 (function () {
 	"use strict";
-	document.addEventListener("load", (event) => {
+	window.addEventListener("load", (event) => {
         document.body.classList.add("loaded");
-        setTimeout(() => {
-            document.body.classList.add("visible");
-        }, 0);
     });
 })();
